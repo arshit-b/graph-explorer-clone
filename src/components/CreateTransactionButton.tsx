6bec01806965efa53eed5db1d7ef7b004347ca0a
arshit-b <arshitbabariya301@gmail.com>
2024-03-18 09:27:25 +0530

    fix styles

--- src/components/CreateTransactionButton.tsx
+++ src/components/CreateTransactionButton.tsx
@@ -2,9 +2,13 @@ import React, {useMemo, useState} from 'react';
 import {
   Box,
   Button,
-  Dialog, DialogActions,
+  Dialog,
+  DialogActions,
   DialogContent,
-  DialogTitle, Input, InputAdornment, Typography,
+  DialogTitle,
+  Input,
+  InputAdornment,
+  Typography,
 } from '@mui/material';
 
 import {Add} from '@mui/icons-material';
@@ -14,7 +18,7 @@ import {User} from 'src/types';
 import {makeTransactionObject} from 'src/utils';
 
 const CreateTransactionButton = () => {
-  const {userList, registerTransaction} = useData()
+  const {userList, registerTransaction} = useData();
 
   const [fromUser, setFromUser] = useState<User | null>(null);
 
@@ -24,34 +28,40 @@ const CreateTransactionButton = () => {
 
   const [amount, setAmount] = useState(0);
 
-
   const fromUserOptions = useMemo(() => {
-    return userList.filter((user) => !toUser || user.address !== toUser.address)
+    return userList.filter(
+      (user) => !toUser || user.address !== toUser.address,
+    );
   }, [toUser, userList]);
 
   const toUserOptions = useMemo(() => {
-    return userList.filter((user) => !fromUser || user.address !== fromUser.address)
+    return userList.filter(
+      (user) => !fromUser || user.address !== fromUser.address,
+    );
   }, [fromUser, userList]);
-
 
   const handleChangeFromUser = (event: React.SyntheticEvent, user: User) => {
     event.stopPropagation();
     setFromUser(user);
-
-  }
+  };
   const handleChangeToUser = (event: React.SyntheticEvent, user: User) => {
     event.stopPropagation();
     setToUser(user);
-  }
+  };
 
   const handleCreateTransaction = () => {
-    registerTransaction(makeTransactionObject(fromUser.address, toUser.address, amount))
-  }
+    registerTransaction(
+      makeTransactionObject(fromUser.address, toUser.address, amount),
+    );
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
   };
-  const handleClose = (event: React.SyntheticEvent<unknown>, reason?: string) => {
+  const handleClose = (
+    event: React.SyntheticEvent<unknown>,
+    reason?: string,
+  ) => {
     if (reason !== 'backdropClick') {
       setAmount(0);
       setToUser(null);
@@ -61,34 +71,56 @@ const CreateTransactionButton = () => {
   };
   return (
     <div>
-      <Button
-        sx={{color: '#fff'}}
-        onClick={handleClickOpen}>
+      <Button sx={{color: '#fff'}} onClick={handleClickOpen}>
         <Add />
         Transaction
       </Button>
-      <Dialog keepMounted={false} disableEscapeKeyDown open={open} onClose={handleClose}>
+      <Dialog
+        keepMounted={false}
+        disableEscapeKeyDown
+        open={open}
+        onClose={handleClose}>
         <DialogTitle>Create transaction</DialogTitle>
-        <DialogContent>
-          <Box component="form" className={'py-4'}>
-            <SelectUser className={'mt-4'} value={fromUser} onChange={handleChangeFromUser} options={fromUserOptions} label={'From'}/>
-            <SelectUser className={'mt-4'} value={toUser} onChange={handleChangeToUser} options={toUserOptions} label={'To'}/>
+        <DialogContent className={'md:min-w-80'}>
+          <Box component="form">
+            <SelectUser
+              className={'mt-4'}
+              value={fromUser}
+              onChange={handleChangeFromUser}
+              options={fromUserOptions}
+              label={'From'}
+            />
+            <SelectUser
+              className={'mt-4'}
+              value={toUser}
+              onChange={handleChangeToUser}
+              options={toUserOptions}
+              label={'To'}
+            />
             <Input
               type={'number'}
               value={amount ? amount.toString() : ''}
               onChange={(event) => {
                 const value = event.target.value;
-                setAmount(Math.abs(Number(value)))
+                setAmount(Math.abs(Number(value)));
               }}
-              slotProps={{ root: {className: 'p-2 mt-4'}, input: {min: 1} }}
+              slotProps={{root: {className: 'p-2 mt-4'}, input: {min: 1}}}
               placeholder={'Amount'}
-              startAdornment={<InputAdornment className={'mr-2'} position={'start'}><Typography className={'text-black'}>$</Typography></InputAdornment>}
+              startAdornment={
+                <InputAdornment className={'mr-2'} position={'start'}>
+                  <Typography className={'text-black'}>$</Typography>
+                </InputAdornment>
+              }
             />
           </Box>
         </DialogContent>
         <DialogActions onClick={handleClose}>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button disabled={!fromUser || !toUser || !amount} onClick={handleCreateTransaction}>Create</Button>
+          <Button
+            disabled={!fromUser || !toUser || !amount}
+            onClick={handleCreateTransaction}>
+            Create
+          </Button>
         </DialogActions>
       </Dialog>
     </div>
--- src/components/SelectUser.tsx
+++ src/components/SelectUser.tsx
@@ -1,33 +1,35 @@
 import React from 'react';
-import {Autocomplete, AutocompleteProps, Box, TextField, Typography} from '@mui/material';
-import {Person} from '@mui/icons-material';
+import {Autocomplete, AutocompleteProps, TextField} from '@mui/material';
 import {User} from 'src/types';
-import Users from 'src/pages/users';
 import UserItem from 'src/components/UserItem';
 
-type Props <T extends User> = {
-	label: string;
+type Props<T extends User> = {
+  label: string;
 } & Omit<AutocompleteProps<T, any, any, any>, 'renderInput' | 'renderOption'>;
 
-const SelectUser = <T extends User>({options, label, value, ...restProps}: Props<T>) => {
-	// @ts-ignore
-	return (
-		<Autocomplete
-			className={'min-w-80'}
-			value={value}
-			isOptionEqualToValue={(option, value) => {
-				if (typeof option === 'string' || value == null) return false;
-				// @ts-ignore
-				return option?.address === value?.address
-			}}
-			options={options.map((user) => ({...user, label: user.name}))}
-			renderOption={(props: any, user, state) => (
-				<UserItem user={user} {...props}/>
-			)}
-			renderInput={(params) => <TextField {...params} label={label} />}
-			{...restProps}
-		/>
-	);
-}
+const SelectUser = <T extends User>({
+  options,
+  label,
+  value,
+  ...restProps
+}: Props<T>) => {
+  return (
+    <Autocomplete
+      className={'min-w-80'}
+      value={value}
+      isOptionEqualToValue={(option, value) => {
+        if (typeof option === 'string' || value == null) return false;
+        // @ts-ignore
+        return option?.address === value?.address;
+      }}
+      options={options.map((user) => ({...user, label: user.name}))}
+      renderOption={(props: any, user, state) => (
+        <UserItem user={user} truncateAddress {...props} />
+      )}
+      renderInput={(params) => <TextField {...params} label={label} />}
+      {...restProps}
+    />
+  );
+};
 
 export default SelectUser;
